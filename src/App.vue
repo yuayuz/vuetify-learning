7810ecf1c9b34ec415ff7698b2835431ea6e198c
yu <2665764951@qq.com>
2024-08-11 09:33:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav></nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped></style>
