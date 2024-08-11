1e5d89af16280d356267e603985f4558da987b1a
yu <2665764951@qq.com>
2024-08-11 20:24:18 +0800

    add mdi

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,14 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import '@mdi/font/css/materialdesignicons.css'
+
 import './index.css'
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi'
+  },
   components,
   directives
 })
